088a7683e068699bc6c25dfa54b1e6af532055f4
Kenny Rch <ken@99nth.com>
2026-03-19 08:55:21 +0300

    feat: Implement token minting

--- src/Ownft.sol
+++ src/Ownft.sol
@@ -8,5 +8,19 @@ import {ERC721Enumerable} from "@openzeppelin/contracts/token/ERC721/extensions/
 /// @title Ownft, an NFT contract
 /// @notice Allows anyone to mint their own NFT by supplying a URI to the NFT
 contract Ownft is ERC721Enumerable {
+    struct NftMetadata {
+        string description;
+        string imageUri;
+    }
+    uint256 private s_tokenCounter;
+    mapping(uint256 => NftMetadata) private s_tokenIdToNftMeta;
+
     constructor() ERC721("Ownft", "OFT") {}
+
+    function mintNft(string calldata description, string calldata imageUri) public payable {
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenIdToNftMeta[s_tokenCounter] = NftMetadata({description: description, imageUri: imageUri});
+        s_tokenCounter += 1;
+    }
+
 }
--- test/unit/OwnftTest.t.sol
+++ test/unit/OwnftTest.t.sol
@@ -8,6 +8,8 @@ import {Ownft} from "src/Ownft.sol";
 contract OwnftTest is Test {
     Ownft public ownft;
 
+    address USER = makeAddr("user");
+
     function setUp() public {
         ownft = new Ownft();
     }
@@ -15,4 +17,19 @@ contract OwnftTest is Test {
     function testInitialSupply() public view {
         assertEq(ownft.totalSupply(), 0);
     }
+
+    function testCanMintToken() public {
+        string memory description = "My NFT Description";
+        string memory imageUri = "https://ipfs.io/ipfs/someipfscid";
+        vm.prank(USER);
+
+	// mint nft
+	ownft.mintNft(description, imageUri);
+
+	// supply should have increased
+        assertEq(ownft.totalSupply(), 1);
+    }
+
+    // should be able to retrieve nft metadata uri by token id
+    
 }
