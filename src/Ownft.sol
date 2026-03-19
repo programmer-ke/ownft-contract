94cb8124ee9db872b346e241cb92eb8a4bdcfe2c
Kenny Rch <ken@99nth.com>
2026-03-19 10:40:27 +0300

    feat: Enable minting of nfts from image URIs

--- src/Ownft.sol
+++ src/Ownft.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.33;
 
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {ERC721Enumerable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
+import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
 
 /// @title Ownft, an NFT contract
 /// @notice Allows anyone to mint their own NFT by supplying a URI to the NFT
@@ -23,4 +25,28 @@ contract Ownft is ERC721Enumerable {
         s_tokenCounter += 1;
     }
 
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        NftMetadata storage nftMetadata = s_tokenIdToNftMeta[tokenId];
+        string memory nftName = string.concat(name(), " #", Strings.toString(tokenId));
+        string memory encodedMetadata =
+            Base64.encode(createMetadataJson(nftName, nftMetadata.description, nftMetadata.imageUri));
+        string memory metadataUri = string.concat(_jsonB64BaseUri(), encodedMetadata);
+        return metadataUri;
+    }
+
+    function createMetadataJson(string memory name, string memory description, string memory imageUri)
+        public
+        pure
+        returns (bytes memory)
+    {
+        bytes memory jsonMetadata = abi.encodePacked(
+            '{"name": "', name, '", "description": "', description, '", "image": "', imageUri, '"}'
+        );
+
+        return jsonMetadata;
+    }
+
+    function _jsonB64BaseUri() private pure returns (string memory) {
+        return "data:application/json;base64,";
+    }
 }
--- test/unit/OwnftTest.t.sol
+++ test/unit/OwnftTest.t.sol
@@ -2,8 +2,9 @@
 
 pragma solidity ^0.8.33;
 
-import {Test} from "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
 import {Ownft} from "src/Ownft.sol";
+import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
 
 contract OwnftTest is Test {
     Ownft public ownft;
@@ -23,13 +24,41 @@ contract OwnftTest is Test {
         string memory imageUri = "https://ipfs.io/ipfs/someipfscid";
         vm.prank(USER);
 
-	// mint nft
-	ownft.mintNft(description, imageUri);
+        // mint nft
+        ownft.mintNft(description, imageUri);
 
-	// supply should have increased
+        // supply should have increased
         assertEq(ownft.totalSupply(), 1);
     }
 
-    // should be able to retrieve nft metadata uri by token id
-    
+    function testValidMetadataJsonCreated() public view {
+        string memory description = "My NFT Description";
+        string memory imageUri = "https://ipfs.io/ipfs/someipfscid";
+        string memory name = "Ownft #123";
+        string memory expectedJson =
+            '{"name": "Ownft #123", "description": "My NFT Description", "image": "https://ipfs.io/ipfs/someipfscid"}';
+
+        bytes memory jsonMetadata = ownft.createMetadataJson(name, description, imageUri);
+
+        assertEq(keccak256(bytes(expectedJson)), keccak256(jsonMetadata));
+    }
+
+    function testCanRetrieveTokenMetadataUri() public {
+        string memory description = "My NFT Description";
+        string memory imageUri = "https://ipfs.io/ipfs/someipfscid";
+        string memory name = "Ownft #0";
+
+        // prepare the expected b64 encoded NFT Metadata Uri
+        string memory encodedMetadata = Base64.encode(ownft.createMetadataJson(name, description, imageUri));
+        string memory expectedMetadataUri = string(abi.encodePacked("data:application/json;base64,", encodedMetadata));
+
+        console.log(expectedMetadataUri);
+
+        // mint nft and retrieve Uri
+        vm.prank(USER);
+        ownft.mintNft(description, imageUri);
+        string memory tokenMetadataUri = ownft.tokenURI(0);
+
+        assertEq(keccak256(bytes(tokenMetadataUri)), keccak256(bytes(expectedMetadataUri)));
+    }
 }
